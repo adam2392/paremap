eb12dbb06250a2ee00d36d385594a298242a4b0d
Adam Li <adam2392@gmail.com>
2016-05-20 20:41:49 -0400

    Created the across blocks reinstatement analysis.

--- buildAcrossDiffPairFeatureMat.m
+++ buildAcrossDiffPairFeatureMat.m
@@ -1,53 +1,48 @@
-function [diffPairFeatureMat1, diffPairFeatureMat2 ] = buildDiffPairFeatureMat(diffWordGroup, sessionFirstBlockDir, sessionSecondBlockDir)
+function [pairFeatureMat1, pairFeatureMat2 ] = buildDiffPairFeatureMat(wordGroup, sessionFirstBlockDir, sessionSecondBlockDir)
     % loop through different words
-    diffPairFeatureMat1 = [];
-    diffPairFeatureMat2 = [];
-    for iWord=1:length(diffWordGroup),
-        wordone = diffWordGroup{iWord}{1};
-        wordtwo = diffWordGroup{iWord}{2};
+    pairFeatureMat1 = [];
+    pairFeatureMat2 = [];
+    
+    % loop through word groups (e.g. same, reverse, probe, different, target)
+    for iPair=1:length(wordGroup),
+        pairone = wordGroup{iPair}{1};
+        pairtwo = wordGroup{iPair}{2};
 
-        %%- create directories to the channel files per word pair
-        wordoneDir = fullfile(sessionBlockDir, wordone);
-        filesone = dir(fullfile(wordoneDir));
-        filesone = {filesone(3:end).name};
-        wordtwoDir = fullfile(sessionBlockDir, wordtwo);
-        filestwo = dir(fullfile(wordtwoDir));
-        filestwo = {filestwo(3:end).name};
+        % create directories to the channel files for this word in this
+        % block
+        paironeDir = fullfile(sessionFirstBlockDir, pairone);
+        paironeFiles = dir(fullfile(paironeDir));
+        paironeFiles = {paironeFiles(3:end).name};
+        pairtwoDir = fullfile(sessionSecondBlockDir, pairtwo);
+        pairtwoFiles = dir(fullfile(pairtwoDir));
+        pairtwoFiles = {pairtwoFiles(3:end).name};
 
-        %%- loop through channels of data
-        firstPairFeatureMat = [];
+        %%- Loop through channels of data
+        firstPairFeatureMat = []; % eventsXfeaturesXtime
         secondPairFeatureMat = [];
-        for iChan=1:length(filesone) % loop through and open up all channels
-            % Load in the data struct for each word pair per channel
-            fileOnePath = fullfile(wordoneDir, filesone{iChan});
-            dataOne = load(fileOnePath);
-            dataOne = dataOne.data;
-            fileTwoPath = fullfile(wordtwoDir, filestwo{iChan});
-            dataTwo = load(fileTwoPath);
-            dataTwo = dataTwo.data;
+        for iChan=1:length(paironeFiles) 
+            paironeFilePath = fullfile(paironeDir, paironeFiles{iChan});
+            pairtwoFilePath = fullfile(pairtwoDir, pairtwoFiles{iChan});
 
-%             dataOne.powerMatZ = dataOne.powerMatZ(:,:,1:size(dataOne.powerMatZ,3)-1);
-%             dataTwo.powerMatZ = dataTwo.powerMatZ(:,:,1:size(dataTwo.powerMatZ,3)-1);
-            
-            
-            % concatenate all the freq. vectors that are already 500 ms
-            % windowed and 100 ms overlap
-            if isempty(firstPairFeatureMat),
-                firstPairFeatureMat = dataOne.powerMatZ;
-                secondPairFeatureMat = dataTwo.powerMatZ;
+            dataPairOne = load(paironeFilePath);
+            dataPairOne = dataPairOne.data;
+            dataPairTwo = load(pairtwoFilePath);
+            dataPairTwo = dataPairTwo.data;
+
+            %%- concatenate all freq. vectors
+            if isempty(firstPairFeatureMat)
+                firstPairFeatureMat = dataPairOne.powerMatZ;
+                secondPairFeatureMat = dataPairTwo.powerMatZ;
             else
-                firstPairFeatureMat = cat(2, firstPairFeatureMat, dataOne.powerMatZ);
-                secondPairFeatureMat = cat(2, secondPairFeatureMat, dataTwo.powerMatZ);
+                firstPairFeatureMat = cat(2, firstPairFeatureMat, dataPairOne.powerMatZ);
+                secondPairFeatureMat = cat(2, secondPairFeatureMat, dataPairTwo.powerMatZ);
             end
         end
-%             disp('different pair feature mats')
-%             size(firstPairFeatureMat)
-%             size(secondPairFeatureMat)
-        %%- build events X features X time matrix with events being
-        %%lined up to be compared
-        for i=1:size(firstPairFeatureMat, 1),
-            diffPairFeatureMat1 = cat(1, diffPairFeatureMat1, repmat(firstPairFeatureMat(i,:,:), size(secondPairFeatureMat, 1), 1, 1));
-            diffPairFeatureMat2 = cat(1, diffPairFeatureMat2, secondPairFeatureMat(:, :, :));
+
+        %%- build events X features X time matrix
+        for i=1:size(firstPairFeatureMat, 1)
+            pairFeatureMat1 = cat(1, pairFeatureMat1, repmat(firstPairFeatureMat(i,:,:), size(secondPairFeatureMat, 1), 1, 1));
+            pairFeatureMat2 = cat(1, pairFeatureMat2, secondPairFeatureMat(:, :, :));
         end
-    end % end of loop through different words
+    end
 end
