37536c42ce7dea3ce2fb58ff674aed621714f1cd
Adam Li <adam2392@gmail.com>
2016-05-31 14:24:37 -0400

    Changing preprocess analysis of vocalization to check after it.

--- runPreProcess.m
+++ runPreProcess.m
@@ -1,9 +1,12 @@
 % ALParemap_eCog_PreProcess('NIH034', 0, 1);
 % ALParemap_eCog_PreProcess('NIH039', 0, 1);
-ALParemap_eCog_PreProcess('NIH034', 0, 1);
-ALParemap_eCog_PreProcess('NIH039', 0, 1);
+% ALParemap_eCog_PreProcess('NIH034', 0, 1);
+% ALParemap_eCog_PreProcess('NIH039', 0, 1);
 
 % ALParemap_PreProcessParallel('NIH034', 0, 1);
 % ALParemap_PreProcessParallel('NIH039', 0, 1);
 % ALParemap_PreProcessParallel('NIH034', 0, 1);
 % ALParemap_PreProcessParallel('NIH039', 0, 1);
+
+ALParemap_eCog_PreProcess('NIH034', 1, 0);
+ALParemap_eCog_PreProcess('NIH039', 1, 0);
